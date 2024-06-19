bf5cdbe0279a0ed564e0be01f428cfee997d4c8e
Julian Braha <julianbraha@gmail.com>
2024-06-19 15:40:31 +0100

    Adjust wrapper script following testing

--- pgo_worker_wrapper.py
+++ pgo_worker_wrapper.py
@@ -23,15 +23,60 @@ PROFILE_DIRECTORY = os.environ.get("PROFILE_DIRECTORY")
 if PROFILE_DIRECTORY is None:
     PROFILE_DIRECTORY = "./target/pgo-profiles/"
 
+# upload the pgo file to the gcs bucket
+def upload_pgo_file_to_gcs(file_path):
+    print("Creating the GCS client...")
+    storage_client = storage.Client(project="immutable-418115")
 
+    # Get the bucket
+    print("Connecting to the bucket...")
+    bucket = storage_client.bucket(GCS_UPLOAD_BUCKET)
+
+    print("Uploading the file to the bucket...")
+    # Create a new blob and upload the file's content
+    file_name = os.path.basename(file_path)
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(file_path)
+    print("Finished. Shutting down...")
+
+def cleanup_pgo_run():
+    # continually checks if the PGO file has been generated before uploading to GCS
+    print("Checking for a .profraw file...")
+    files = os.listdir(PROFILE_DIRECTORY)
+    while len(files) < 1:
+        print("No .profraw file found. Waiting 1 second then checking again...")
+        print("The profiling directory is:", PROFILE_DIRECTORY)
+        time.sleep(1)
+        files = os.listdir(PROFILE_DIRECTORY)
+
+    if len(files) > 1:
+        print("FATAL: more than 1 file in the profiling directory:", files)
+        print("The profiling directory is:", PROFILE_DIRECTORY)
+        print("Exiting...")
+        sys.exit(1)
+    else:
+        print("Found the .profraw file. Uploading to GCS bucket:", GCS_UPLOAD_BUCKET)
+        pgo_file = files[0]
+        if pgo_file.endswith(".profraw"):
+            full_pgo_file_path = PROFILE_DIRECTORY + "/" + pgo_file
+            upload_pgo_file_to_gcs(full_pgo_file_path)
+            sys.exit(0)
+        else:
+            print("FATAL: unexpected file extension (should be .profraw) in the profiling directory. File is:", pgo_file)
+            print("The profiling directory is:", PROFILE_DIRECTORY)
+            print("Exiting...")
+            sys.exit(1)
+
+# sends SIGTERM signals to the binary if SIGINT or SIGTERM are received.
+#   NOTE: if you want to kill this wrapper instead of the inner binary, use SIGHUP or `kill -9`
 def signal_handler(sig, frame):
     ''' for handling interrupts (currently SIGINT and SIGTERM) '''
     if sig == signal.SIGINT:
-        print('Interrupted by user (SIGINT)')
-        process.send_signal(signal.SIGINT)
+        print('Interrupted by user (SIGINT). Passing along a SIGTERM to pgo-worker instead...')
+        process.send_signal(signal.SIGTERM)
         cleanup_pgo_run()
     elif sig == signal.SIGTERM:
-        print('Termination signal received (SIGTERM)')
+        print('Termination signal received (SIGTERM). Passing it along to pgo-worker...')
         process.send_signal(signal.SIGTERM)
         cleanup_pgo_run()
     else:
@@ -47,37 +92,6 @@ process = subprocess.Popen(WORKER_PATH)
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
-
-# upload the pgo file to the gcs bucket
-def upload_pgo_file_to_gcs(file_path):
-    storage_client = storage.Client()
-
-    # Get the bucket
-    bucket = storage_client.bucket(GCS_UPLOAD_BUCKET)
-
-    # Create a new blob and upload the file's content
-    file_name = os.path.basename(file_path)
-    blob = bucket.blob(file_name)
-    blob.upload_from_filename(file_path)
-
-def cleanup_pgo_run():
-    # continually checks if the PGO file has been generated before uploading to GCS
-    files = os.listdir(PROFILE_DIRECTORY)
-    while len(files) < 1:
-        time.sleep(1)
-        files = os.listdir(PROFILE_DIRECTORY)
-
-    if len(files) > 1:
-        print("FATAL: more than 1 file in the profiling directory:", files)
-        print("The profiling directory is:", PROFILE_DIRECTORY)
-        print("Exiting...")
-        sys.exit(1)
-    else:
-        pgo_file = files[0]
-        if pgo_file.endswith(".profraw"):
-            upload_pgo_file_to_gcs(pgo_file)
-        else:
-            print("FATAL: unexpected file extension (should be .profraw) in the profiling directory. File is:", pgo_file)
-            print("The profiling directory is:", PROFILE_DIRECTORY)
-            print("Exiting...")
-            sys.exit(1)
+# wait to receive a signal (ensures that the wrapper does not terminate)
+while True:
+    pass
